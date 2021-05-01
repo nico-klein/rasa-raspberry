169ca244e320f4f90a44ada80848265b58af04e7
Ubuntu <ubuntu@pi.fritz.box>
2021-05-01 15:29:42 +0000

    fixes

--- start_bot1_servers.sh
+++ start_bot1_servers.sh
@@ -1,6 +1,4 @@
 source ./venv/bin/activate
 cd bot1
-# only needed on pi
-# on other linux or max start wirh "rasa ..."
-python3 call_rasa.py run &
-python3 call_rasa.py run actions &
+rasa run &
+rasa run actions &
--- start_bot1_shell.sh
+++
@@ -1,6 +0,0 @@
-source ./venv/bin/activate
-cd bot1
-# only needed on pi
-# on other linux or max start wirh "rasa ..."
-python3 call_rasa.py shell
-# python3 call_rasa.py run actions &
